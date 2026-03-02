bfa2944c37d9b9b53534966700070654e702d8d6
Anni Chiganangana <48432614+Annie-Chigana@users.noreply.github.com>
2026-03-02 20:04:18 +0200

    initial commit

---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div className="header-body">
+        <div className="header-left">Contact</div>
+        <div className="header-center">Annie Chiganangana</div>
+        <div className="header-right">
+            <ul>
+                <li>LinkedIn</li>
+                <li>GitHub</li>
+                <li>Email</li>
+            </ul>
+        </div>
+        </div>
+    )
+}
+export default Header;
